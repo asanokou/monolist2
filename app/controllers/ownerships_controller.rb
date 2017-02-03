970fba92b755e90ea78962285e91eebdf5176c94
asanokou <kou.3.0301@icloud.com>
2017-02-03 09:47:56 +0000

    add have or want button

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -11,7 +11,9 @@ class OwnershipsController < ApplicationController
     # itemsテーブルに存在しない場合は楽天のデータを登録する。
     if @item.new_record?
       # TODO 商品情報の取得 RakutenWebService::Ichiba::Item.search を用いてください
-      items = {}
+      items = RakutenWebService::Ichiba::Item.search(
+        itemCode: params[:item_code]
+      )
 
       item                  = items.first
       @item.title           = item['itemName']
@@ -20,6 +22,14 @@ class OwnershipsController < ApplicationController
       @item.large_image     = item['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
       @item.detail_page_url = item['itemUrl']
       @item.save!
+    end
+    
+    if(params[:type] =="Have")
+      current_user.have(@item)
+    end
+    
+    if(params[:type] =="Want")
+      current_user.want(@item)
     end
 
     # TODO ユーザにwant or haveを設定する
@@ -31,7 +41,14 @@ class OwnershipsController < ApplicationController
 
   def destroy
     @item = Item.find(params[:item_id])
-
+    
+    if(params[:type] =="Have")
+      current_user.unhave(@item)
+    end
+    
+    if(params[:type] =="Want")
+      current_user.unwant(@item)
+    end
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
